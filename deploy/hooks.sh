7d225df236b0ccbef0912e19e5e0422a9884f9ae
khc196 <khc146@gmail.com>
2020-02-19 05:18:00 +0900

    Update WebHook Script

--- deploy/hooks.sh
+++ deploy/hooks.sh
@@ -4,4 +4,4 @@ git checkout -t origin/deploy
 git pull origin deploy
 pip install -r requirements.txt 
 #TODO: just for test now
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
