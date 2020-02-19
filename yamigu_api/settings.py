7cbb3afbb2899455d9aa1c316ff26707d7447820
khc196 <khc146@gmail.com>
2020-02-19 16:58:34 +0900

    remove rest-swagger

--- yamigu_api/settings.py
+++ yamigu_api/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
 
     # my apps
     'core',
@@ -130,7 +129,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
--- yamigu_api/urls.py
+++ yamigu_api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from authorization.urls import urlpatterns as auth_url_patterns
 from core.urls import urlpatterns as core_url_patterns
 from deploy.urls import urlpatterns as deploy_url_patterns
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authorization/', include(auth_url_patterns)),
