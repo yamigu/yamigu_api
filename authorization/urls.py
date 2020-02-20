c4e9f85ff846910a870a4714e93632eab602bc1e
khc196 <khc146@gmail.com>
2020-02-20 16:47:12 +0900

    Authorization - urls: Update apple login url

--- authorization/urls.py
+++ authorization/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
+from allauth.socialaccount.providers.apple.views import AppleOAuth2LoginView
+
 
 urlpatterns = [
     path('oauth/kakao/', KakaoLoginView.as_view()),
-    path('oauth/apple/', AppleLoginView.as_view()),
+    path('oauth/apple/', AppleOAuth2LoginView.as_view()),
 
     path('user/info/', UserInfoView.as_view()),
     path('user/profile_image/', ProfileImageView.as_view()),
