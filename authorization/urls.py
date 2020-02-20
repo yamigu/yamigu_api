c519afb87ecd2e047b9b3ee44235bc52965e546d
khc196 <khc146@gmail.com>
2020-02-20 16:53:12 +0900

    Authorization - urls: revert changes

--- authorization/urls.py
+++ authorization/urls.py
@@ -2,12 +2,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
-from allauth.socialaccount.providers.apple.views import AppleOAuth2LoginView
 
 
 urlpatterns = [
     path('oauth/kakao/', KakaoLoginView.as_view()),
-    path('oauth/apple/', AppleOAuth2LoginView.adapter_view),
+    path('oauth/apple/', AppleLoginView.as_view()),
 
     path('user/info/', UserInfoView.as_view()),
     path('user/profile_image/', ProfileImageView.as_view()),
