73bb0a4d4109e6ed0b9fe039c1a9b479bfe6fc9e
khc196 <khc146@gmail.com>
2020-02-22 03:52:34 +0900

    Update hooks script

--- deploy/hooks.sh
+++ deploy/hooks.sh
@@ -3,5 +3,7 @@ echo "push"
 git checkout deploy
 git pull origin deploy
 pip install -r requirements.txt 
+python manage.py makemigrations
+python manage.py migrate
 #TODO: just for test now
 #python manage.py runserver 0.0.0.0:8000
