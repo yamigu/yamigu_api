e24de608d7d1e651a411c462857c8f2449a6082f
khc196 <khc146@gmail.com>
2020-03-08 03:59:00 +0900

    Admin User Form update

--- authorization/admin.py
+++ authorization/admin.py
@@ -1,12 +1,109 @@
 from django.contrib import admin
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.safestring import mark_safe
 from .models import *
-admin.site.register(User)
+from core.models import *
+
+from django.urls import reverse
+
 admin.site.register(Location)
 admin.site.register(ProfileImage)
 admin.site.register(IdentityVerification)
 admin.site.register(BelongVerification)
 admin.site.register(BVImage)
 admin.site.register(FirebaseToken)
+
+
+class IVInline(admin.TabularInline):
+    model = IdentityVerification
+
+
+class EditLinkToInlineObject(object):
+    def edit_link(self, instance):
+        url = reverse('admin:%s_%s_change' % (
+            instance._meta.app_label,  instance._meta.model_name),  args=[instance.pk])
+        if instance.pk:
+            return mark_safe(u'<a href="{u}">edit</a>'.format(u=url))
+        else:
+            return ''
+
+
+class BVImageInline(admin.TabularInline):
+    model = BVImage
+
+
+class BVInline(EditLinkToInlineObject, admin.TabularInline):
+    model = BelongVerification
+
+    readonly_fields = ('bv_image',)
+
+    def bv_image(self, obj):
+        return mark_safe('<img src="{url}" width="320px" />'.format(url=obj.image.last().data.src))
+
+
+class ProfileImageInline(admin.TabularInline):
+    model = ProfileImage
+    ordering = ('number',)
+    exclude = ('data', 'number')
+    readonly_fields = ('profile_image',)
+    max_num = 1
+
+    def profile_image(self, obj):
+        return mark_safe('<img src="{url}" width="320px" />'.format(url=obj.data.src))
+
+
+class RecvChatInline(admin.TabularInline):
+    model = Chat
+    fk_name = "sender"
+    max_num = 0
+
+
+class SentChatInline(admin.TabularInline):
+    model = Chat
+    fk_name = "receiver"
+    max_num = 0
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('nickname', 'uid', 'real_name')
+    readonly_fields = ('verified', 'num_of_yami',
+                       'num_of_free')
+    list_display = ('uid', 'nickname', 'personal_info', 'belong',
+                    'verified', 'num_of_yami', 'num_of_free')
+    exclude = ('is_admin', 'is_staff', 'is_active', 'password',
+               'groups', 'user_permissions', 'Superuser_status', )
+    inlines = (IVInline, BVInline, ProfileImageInline,
+               RecvChatInline, SentChatInline)
+
+    def personal_info(self, user):
+        try:
+            return '{}({})'.format(user.iv.realname, user.iv.birthdate)
+        except:
+            return ''
+
+    def verified(self, user):
+        try:
+            if(user.bv.image.count() > 0):
+                if(user.bv.image.last().is_checked):
+                    return '인증 완료'
+                return '인증 진행중'
+        except:
+            return '미인증'
+
+    def belong(self, user):
+        try:
+            return user.bv.belong
+        except:
+            return ''
+
+    def department(self, user):
+        try:
+            return user.bv.department
+        except:
+            return ''
+
+
+admin.site.register(User, UserAdmin)
 # class UserAdmin(admin.ModelAdmin):
 #     list_filter = ('is_certified',)
 #     list_display = ('id', 'uid', 'nickname', 'birth', 'gender_string', 'real_name', 'is_certified_string', 'yami')
