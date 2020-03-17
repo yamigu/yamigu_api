77972eeec26042e44e14c5d116f3829a37567a84
khc196 <khc146@gmail.com>
2020-03-17 16:15:58 +0900

    update hooks.sh

--- deploy/hooks.sh
+++ deploy/hooks.sh
@@ -2,8 +2,8 @@
 echo "push"
 git checkout deploy
 git pull origin deploy
-pip install -r requirements.txt 
-/ubuntu/home/yamigu_api_venv/bin/python manage.py makemigrations
-/ubuntu/home/yamigu_api_venv/bin/python manage.py migrate
+/home/ubuntu/yamigu_api_venv/bin/pip install -r requirements.txt 
+/home/ubuntu/yamigu_api_venv/bin/python manage.py makemigrations
+/home/ubuntu/yamigu_api_venv/bin/python manage.py migrate
 #TODO: just for test now
 #python manage.py runserver 0.0.0.0:8000
