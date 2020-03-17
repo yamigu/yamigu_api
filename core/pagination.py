e0aedc2e8db81a5a60942d585effe53cd86aade6
khc196 <khc146@gmail.com>
2020-03-18 00:24:13 +0900

    fix pagination + shuffle not working

--- core/pagination.py
+++ core/pagination.py
@@ -11,10 +11,8 @@ class SmallPagesPagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
-            },
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
             'total': self.page.paginator.count,
             'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
             'page_size': int(self.request.GET.get('page_size', self.page_size)),
